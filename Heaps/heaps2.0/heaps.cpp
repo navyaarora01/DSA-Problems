7e31f3bba17c2e8f1d74aa45b11ca8fa98fab96a
Navya <arora.navya2003@gmail.com>
2022-06-09 23:51:45 +0530

    commit

--- Heaps/heaps2.0/heaps.cpp
+++ Heaps/heaps2.0/heaps.cpp
@@ -102,12 +102,12 @@ void heapify(int arr[], int n, int i)
    
     
     
-    void heapSort(int arr[], int n)
-    {
-        buildHeap(arr , n);
-        for(int i=n-1;i>0;i--){
-            swap(arr[0], arr[n-1]);
-            heapify(arr , i , 0);
-        }
-        //code here
-    }
+    // void heapSort(int arr[], int n)
+    // {
+    //     buildHeap(arr , n);
+    //     for(int i=n-1;i>0;i--){
+    //         swap(arr[0], arr[n-1]);
+    //         heapify(arr , i , 0);
+    //     }
+    //     //code here
+    // }
