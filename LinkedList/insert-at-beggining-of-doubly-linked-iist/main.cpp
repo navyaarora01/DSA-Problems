a13aadd7e996709eccdf15bf514a1aa0932ecc10
Navya <arora.navya2003@gmail.com>
2022-06-25 22:25:59 +0530

    commit

--- LinkedList/insert-at-beggining-of-doubly-linked-iist/main.cpp
+++ LinkedList/insert-at-beggining-of-doubly-linked-iist/main.cpp
@@ -19,7 +19,7 @@ Node *insertAtBegin(Node *head , int x){
    return temp;
 }
 void printList(Node *head){
-    Node *curr = head;
+    Node *curr =  head;
     while(curr!=NULL){
         cout<<(curr->data)<<endl;
         curr = curr->next;
