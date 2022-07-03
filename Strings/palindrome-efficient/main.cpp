67c4696f345d81dfeccf1fe4fca8d516b06138f6
Navya <arora.navya2003@gmail.com>
2022-07-03 23:46:44 +0530

    commit

--- Strings/palindrome-efficient/main.cpp
+++ Strings/palindrome-efficient/main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 bool isPal(string &str){
     int begin = 0;
     int end = str.length()-1;
+    
     while(begin<end){
+
         if(str[begin]!=str[end]){
             return false;
         }
