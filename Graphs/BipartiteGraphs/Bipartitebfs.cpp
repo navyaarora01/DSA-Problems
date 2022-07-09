37010be6668ae243439a0fd591ae6832ad19c14b
Navya <arora.navya2003@gmail.com>
2022-07-09 11:20:26 +0530

    commit

--- Graphs/BipartiteGraphs/Bipartitebfs.cpp
+++ Graphs/BipartiteGraphs/Bipartitebfs.cpp
@@ -1,12 +1,57 @@
 #include<bits/stdc++.h>
-using namesoace std;
+using namespace std;
+
+bool bipartiteBFS(int src , vector<int> adj[] , vector<int> color[]){
+    queue<int> q;
+    q.push(src);
+    color[src] = 1;
+    while(!q.empty()){
+        int curr = q.front();
+
+        for(int it : adj[curr]){
+            if(color[it] == -1){
+                color[it ] = 1-color[curr];
+                q.push(it);
+            }
+            else if(color[it] == color[curr]){
+                return false;
+            }
+        }
+        q.pop();
+       
+    } 
+    return true;
+}
+
+bool bipartite( vector<int> adj[] , int n){
+    vector<int> color;
+    color.assign(n , -1);
+
+    for(int i=0;i<n;i++){
+        if(color[i] == -1){
+            if(!bipartiteBFS(i , adj , color)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
  int main(){
     int n , m;   //node and edges
     cin>>n>>m;
-    vector<int> adj;
+    vector<int> adj[n];
     for(int i=0;i<m;i++){
         int u , v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
+    if(bipartite(adj , n)){
+        cout<<"TRUE"<<endl;
+    }
+    else{
+        cout<<"FALSE"<<endl;
+    }
  }
+
