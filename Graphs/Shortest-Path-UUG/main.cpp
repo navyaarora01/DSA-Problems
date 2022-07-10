1ca5600ba400d547e1c0ec8c36a199e46b5cb7b1
Navya <arora.navya2003@gmail.com>
2022-07-10 09:33:35 +0530

    commit

---
+++ Graphs/Shortest-Path-UUG/main.cpp
@@ -0,0 +1,42 @@
+// shortest path in undirected and unweighted graph
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+void BFS(vector<int> adj[], int N, int src) 
+{ 
+	int dist[N];
+	for(int i = 0;i<N;i++) dist[i] = INT16_MAX; 
+	queue<int>  q;
+	
+	dist[src] = 0;
+	q.push(src); 
+
+	while(q.empty()==false) 
+	{ 
+		int node = q.front(); 
+		q.pop();
+		 
+		for(auto it:adj[node]){
+		    if(dist[node] + 1 < dist[it]){
+		        dist[it]=dist[node]+1;
+		        q.push(it);
+		    }
+		} 
+	} 
+	for(int i = 0;i<N;i++) cout << dist[i] << " "; 
+	
+} 
+int main(){
+     int n , m;   //node and edges
+    cin>>n>>m;
+    vector<int> adj[n];
+    for(int i=0;i<m;i++){
+        int u , v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    BFS(adj , n , 0);
+}
